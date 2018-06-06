f79e218343c1c39d61db4d3dccd405195d78fb15
0x1abin <270995079@qq.com>
2018-06-06 09:57:14 +0800

    fixed the PWM duty range

--- lib/servo.py
+++ lib/servo.py
@@ -28,7 +28,7 @@ class Servo:
             self.pwm.duty(0)
             return
         us = min(self.max_us, max(self.min_us, us))
-        duty = us * 1024 * self.freq // 1000000
+        duty = us * 100 * self.freq // 1000000
         self.pwm.duty(duty)
 
     def write_angle(self, degrees=None, radians=None):
